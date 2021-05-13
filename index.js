d053f0214e5e229f5b7cc8ccb8547ae1728aa4bf
Ryan Erricson <ryanerricson@gmail.com>
2021-05-13 15:08:16 -0400

    commit tree 1b188d51b5d92761b814db794320e085295b4da9

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+function mapToNegativize(sourceArray){
+    return sourceArray.map(x => x * -1)
+}
+
+function mapToNoChange(sourceArray){
+    return sourceArray.map(x => x)
+}
+
+function mapToDouble(sourceArray){
+    return sourceArray.map(x => x * 2)
+}
+
+function mapToSquare(sourceArray){
+    return sourceArray.map(x => x ** 2)
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+    return sourceArray.reduce((currentTotal, x)=>{
+        return x + currentTotal}, startingPoint)
+}
+
+function reduceToAllTrue(sourceArray) {
+        if (sourceArray.includes(false)) {
+            return false
+        } else {
+            return true
+        }
+    }
+
+    function reduceToAnyTrue(sourceArray) {
+        if (sourceArray.includes(true)) {
+            return true
+        } else {
+            return false
+        }
+    }
